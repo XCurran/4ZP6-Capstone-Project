065269fcfc8b9f3e24b52a19ff9dd3a3d2623fc1
andreiciontea <ciontea@mcmaster.ca>
2017-04-09 21:47:13 -0700

    update

--- databaseTest/drop.sql
+++
@@ -1,6 +0,0 @@
--- connect to database;
-connect to cs3db3;
---++++++++++++++++++++++++++++++++++++++++++++++
--- DROP TABLES 
---++++++++++++++++++++++++++++++++++++++++++++++
-DROP TABLE MEDICATION;
